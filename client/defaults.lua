fc4583b8c4250b2e225cb576ec054ca77bcf8551
Luccboy <58391278+Luccboy@users.noreply.github.com>
2025-08-02 11:35:14 +0200

    Improve vehicle door targeting
    Co-authored-by: Heliumdioxid <69712938+Heliumdioxid@users.noreply.github.com>

--- client/defaults.lua
+++ client/defaults.lua
@@ -1,15 +1,12 @@
 if GetConvarInt('ox_target:defaults', 1) ~= 1 then return end
 
 local api = require 'client.api'
-local GetEntityBoneIndexByName = GetEntityBoneIndexByName
-local GetEntityBonePosition_2 = GetEntityBonePosition_2
-local GetVehicleDoorLockStatus = GetVehicleDoorLockStatus
 
-local bones = {
-    [0] = 'dside_f',
-    [1] = 'pside_f',
-    [2] = 'dside_r',
-    [3] = 'pside_r'
+local bones <const> = {
+    [0] = 'door_dside_f',
+    [1] = 'door_pside_f',
+    [2] = 'door_dside_r',
+    [3] = 'door_pside_r'
 }
 
 ---@param vehicle number
@@ -21,28 +18,6 @@ local function toggleDoor(vehicle, door)
         else
             SetVehicleDoorOpen(vehicle, door, false, false)
         end
-    end
-end
-
----@param entity number
----@param coords vector3
----@param door number
----@param useOffset boolean?
----@return boolean?
-local function canInteractWithDoor(entity, coords, door, useOffset)
-    if not GetIsDoorValid(entity, door) or GetVehicleDoorLockStatus(entity) > 1 or IsVehicleDoorDamaged(entity, door) or cache.vehicle then return end
-
-    if useOffset then return true end
-
-    local boneName = bones[door]
-
-    if not boneName then return false end
-
-    local boneId = GetEntityBoneIndexByName(entity, 'door_' .. boneName)
-
-    if boneId ~= -1 then
-        return #(coords - GetEntityBonePosition_2(entity, boneId)) < 0.5 or
-            #(coords - GetEntityBonePosition_2(entity, GetEntityBoneIndexByName(entity, 'seat_' .. boneName))) < 0.72
     end
 end
 
@@ -61,15 +36,68 @@ RegisterNetEvent('ox_target:toggleEntityDoor', function(netId, door)
     toggleDoor(entity, door)
 end)
 
+local function getRelativeDoorCoords(entity, doorId)
+    local boneIndex <const> = GetEntityBoneIndexByName(entity, bones[doorId])
+    local worldPositionOfBone <const> = GetWorldPositionOfEntityBone(entity, boneIndex)
+    return GetOffsetFromEntityGivenWorldCoords(entity, worldPositionOfBone.x, worldPositionOfBone.y, worldPositionOfBone.z)
+end
+
+local function canInteractWithDoor(entity, doorId)
+    return not cache.vehicle
+        and DoesEntityExist(entity)
+        and DoesEntityHaveDrawable(entity)
+        and GetIsDoorValid(entity, doorId)
+        and not IsVehicleDoorDamaged(entity, doorId)
+        and GetVehicleDoorLockStatus(entity) <= 1
+end
+
+local function isTargetPointingAtDoor(entity, doorId, coords, bone)
+    if not canInteractWithDoor(entity, doorId) then
+        return false
+    end
+
+    if GetVehicleModelNumberOfSeats(GetEntityModel(entity)) > 4 then -- vehicle is not a car (e.g. bus)
+        return bone == GetEntityBoneIndexByName(entity, bones[doorId]) -- return true to apply ox_target default logic for targeting doors
+    end
+
+    local relativeCoords <const> = GetOffsetFromEntityGivenWorldCoords(entity, coords.x, coords.y, coords.z)
+    local relativeCoordsY <const> = (math.floor(relativeCoords.y * 100)) / 100
+
+    -- return false if the targeted door is on the wrong side of the vehicle
+    -- the target is pointing to the driver side if the relative x < 0
+    local isPassengerSide <const> = doorId % 2 == 1
+    if (relativeCoords.x < 0) == isPassengerSide then
+        return false
+    end
+
+    local relativeDoorCoords <const> = getRelativeDoorCoords(entity, doorId)
+
+    -- to determine if the targeted door is a front door, we need to check if the pointer's position is between the bone of the front door and the bone of the rear door
+    -- this is not necessary if the vehicle only has front doors
+    local isFrontDoor <const> = doorId <= 1
+    if isFrontDoor then
+        local rearDoorId <const> = doorId + 2
+        if GetIsDoorValid(entity, rearDoorId) and not IsVehicleDoorDamaged(entity, rearDoorId) then
+            local relativeRearDoorCoords = getRelativeDoorCoords(entity, rearDoorId)
+            return relativeCoordsY <= relativeDoorCoords.y
+                and relativeCoordsY > relativeRearDoorCoords.y
+                and #(relativeCoords - relativeDoorCoords) < 2.5
+        end
+    end
+
+    return relativeCoordsY <= relativeDoorCoords.y
+        and #(relativeCoords - relativeDoorCoords) < 2.5
+end
+    
 api.addGlobalVehicle({
     {
         name = 'ox_target:driverF',
         icon = 'fa-solid fa-car-side',
         label = locale('toggle_front_driver_door'),
-        bones = { 'door_dside_f', 'seat_dside_f' },
+        bones = { 'door_dside_f', 'door_pside_f', 'door_dside_r', 'door_pside_r' },
         distance = 2,
-        canInteract = function(entity, distance, coords, name)
-            return canInteractWithDoor(entity, coords, 0)
+        canInteract = function(entity, distance, coords, name, bone)
+            return isTargetPointingAtDoor(entity, 0, coords, bone)
         end,
         onSelect = function(data)
             onSelectDoor(data, 0)
@@ -79,10 +107,10 @@ api.addGlobalVehicle({
         name = 'ox_target:passengerF',
         icon = 'fa-solid fa-car-side',
         label = locale('toggle_front_passenger_door'),
-        bones = { 'door_pside_f', 'seat_pside_f' },
+        bones = { 'door_dside_f', 'door_pside_f', 'door_dside_r', 'door_pside_r' },
         distance = 2,
-        canInteract = function(entity, distance, coords, name)
-            return canInteractWithDoor(entity, coords, 1)
+        canInteract = function(entity, distance, coords, name, bone)
+            return isTargetPointingAtDoor(entity, 1, coords, bone)
         end,
         onSelect = function(data)
             onSelectDoor(data, 1)
@@ -92,10 +120,10 @@ api.addGlobalVehicle({
         name = 'ox_target:driverR',
         icon = 'fa-solid fa-car-side',
         label = locale('toggle_rear_driver_door'),
-        bones = { 'door_dside_r', 'seat_dside_r' },
+        bones = { 'door_dside_f', 'door_pside_f', 'door_dside_r', 'door_pside_r' },
         distance = 2,
-        canInteract = function(entity, distance, coords)
-            return canInteractWithDoor(entity, coords, 2)
+        canInteract = function(entity, distance, coords, name, bone)
+            return isTargetPointingAtDoor(entity, 2, coords, bone)
         end,
         onSelect = function(data)
             onSelectDoor(data, 2)
@@ -105,10 +133,10 @@ api.addGlobalVehicle({
         name = 'ox_target:passengerR',
         icon = 'fa-solid fa-car-side',
         label = locale('toggle_rear_passenger_door'),
-        bones = { 'door_pside_r', 'seat_pside_r' },
+        bones = { 'door_dside_f', 'door_pside_f', 'door_dside_r', 'door_pside_r' },
         distance = 2,
-        canInteract = function(entity, distance, coords)
-            return canInteractWithDoor(entity, coords, 3)
+        canInteract = function(entity, distance, coords, name, bone)
+            return isTargetPointingAtDoor(entity, 3, coords, bone)
         end,
         onSelect = function(data)
             onSelectDoor(data, 3)
@@ -121,7 +149,7 @@ api.addGlobalVehicle({
         offset = vec3(0.5, 1, 0.5),
         distance = 2,
         canInteract = function(entity, distance, coords)
-            return canInteractWithDoor(entity, coords, 4, true)
+            return canInteractWithDoor(entity, 4)
         end,
         onSelect = function(data)
             onSelectDoor(data, 4)
@@ -134,7 +162,7 @@ api.addGlobalVehicle({
         offset = vec3(0.5, 0, 0.5),
         distance = 2,
         canInteract = function(entity, distance, coords, name)
-            return canInteractWithDoor(entity, coords, 5, true)
+            return canInteractWithDoor(entity, 5)
         end,
         onSelect = function(data)
             onSelectDoor(data, 5)
